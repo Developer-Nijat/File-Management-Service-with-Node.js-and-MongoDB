c87d6e5b67207bee69e7b0c09cab4246be37bd31
Nijat Aliyev <developer.nijat@gmail.com>
2024-12-24 23:07:25 +0400

    deactivated body base64 logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -11,15 +11,16 @@ const errorLogStream = rfs.createStream("error.log", {
   path: "./logs",
 });
 
-morgan.token("body", (req, res) => JSON.stringify(req.body));
+// morgan.token("body", (req, res) => JSON.stringify(req.body));
 morgan.token("reqUrl", (req, res) =>
   req.url ? req.url.split("?")[0] : req.url
 );
 morgan.token("reqParams", (req, res) => JSON.stringify(req.params));
 morgan.token("reqQuery", (req, res) => JSON.stringify(req.query));
 
+// if needed add :body
 const accessLogger = morgan(
-  ":remote-addr | :date[iso] | :method | ':reqUrl' | ':reqQuery' | :reqParams | :body | HTTP/:http-version | :status | :referrer | ':user-agent'",
+  ":remote-addr | :date[iso] | :method | ':reqUrl' | ':reqQuery' | :reqParams | HTTP/:http-version | :status | :referrer | ':user-agent'",
   {
     skip: function (req, res) {
       return res.statusCode > 399; // only log access
